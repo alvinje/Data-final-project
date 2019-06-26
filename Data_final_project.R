7066f6b6813a3e0a94893151a361e0e0e5ffa762
Sandra Martineau <sandramartineau@yahoo.fr>
2019-06-26 12:20:34 +0200

    delete annee_naissance == 1904

--- Data_final_project.R
+++ Data_final_project.R
@@ -32,50 +32,50 @@ colnames(csv)[32] <- "specialisation_pro_1"
 
 # Rename columns with Dplyr package.
 csv <- csv %>% rename(
-  annee_naissance = "Quelle est votre ann�e de naissance ?",
-  genre = "Vous �tes ?",
-  nationalite = "Quel est votre nationalit� ?",
-  departement_travail = "Dans quel d�partement travaillez-vous ?",
+  annee_naissance = "Quelle est votre année de naissance ?",
+  genre = "Vous êtes ?",
+  nationalite = "Quel est votre nationalité ?",
+  departement_travail = "Dans quel département travaillez-vous ?",
   situation_pro = "Quelle est votre situation professionnelle ?",
-  specialisation_pro = "Quelle est votre sp�cialisation professionnelle ?",
-  diplome_actuel =  "Quelle est votre niveau de dipl�me actuel ?",
+  specialisation_pro = "Quelle est votre spécialisation professionnelle ?",
+  diplome_actuel =  "Quelle est votre niveau de diplôme actuel ?",
   tranche_salaire = "Quelle est votre tranche de salaire ?",
   type_bac = "Quel type de Bac avez-vous obtenu ?",
-  annee_dernier_diplome_avant_DC = "En quelle ann�e avez-vous obtenu votre dernier dipl�me avant Digital Campus ?",
-  dernier_diplome_avant_dc = "Quel est votre dernier niveau d'�tude obtenu ?",
-  formation_DC = "Quelle formation avez-vous suivi � Digital Campus ?",
+  annee_dernier_diplome_avant_DC = "En quelle année avez-vous obtenu votre dernier diplôme avant Digital Campus ?",
+  dernier_diplome_avant_dc = "Quel est votre dernier niveau d'étude obtenu ?",
+  formation_DC = "Quelle formation avez-vous suivi à Digital Campus ?",
   ville_formation_DC = "Dans quelle ville avez-vous fait votre formation Digital Campus ?",
-  annee_debut_DC = "En quelle ann�e avez-vous rejoint Digital Campus ?",
-  annee_fin_DC = "En quelle ann�e avez-vous quitt� Digital Campus ?",
-  niveau_avt_dc_crea = "Comment �valuez-vous votre niveau avant Digital Campus dans les domaines suivants? [Cr�atif]",
-  niveau_avt_dc_mkt_com = "Comment �valuez-vous votre niveau avant Digital Campus dans les domaines suivants? [Marketing et communication]",
-  niveau_avt_dc_dev = "Comment �valuez-vous votre niveau avant Digital Campus dans les domaines suivants? [Developpement]",
-  niveau_avt_dc_gest = "Comment �valuez-vous votre niveau avant Digital Campus dans les domaines suivants? [Gestion de projet]",
-  niveau_apr_dc_crea = "Comment �valuez-vous votre niveau apr�s Digital Campus dans les domaines suivants? [Cr�atif]",
-  niveau_apr_dc_mkt_com = "Comment �valuez-vous votre niveau apr�s Digital Campus dans les domaines suivants? [Marketing et communication]",
-  niveau_apr_dc_dev = "Comment �valuez-vous votre niveau apr�s Digital Campus dans les domaines suivants? [Developpement]",
-  niveau_apr_dc_gest = "Comment �valuez-vous votre niveau apr�s Digital Campus dans les domaines suivants? [Gestion de projet]",
-  spe = "Quelle sp�cialit� avez-vous choisi ?",
-  spe_ok = "Votre sp�cialit� a-t-elle r�pondu � vos attentes ?",
-  tps_trouve_alt = "En combien de temps avez-vous trouv� votre alternance ?",
-  sourcing_alt = "Comment avez-vous trouv� votre alternance ?",
-  reco_dc = "Recommanderiez-vous Digital Campus � d'autres personnes sur une �chelle de 0 � 5 ?",
-  recherche_emploi_apr_dc = "Avez-vous cherch� un emploi � la fin de vos �tudes ?",
+  annee_debut_DC = "En quelle année avez-vous rejoint Digital Campus ?",
+  annee_fin_DC = "En quelle année avez-vous quitté Digital Campus ?",
+  niveau_avt_dc_crea = "Comment évaluez-vous votre niveau avant Digital Campus dans les domaines suivants? [Créatif]",
+  niveau_avt_dc_mkt_com = "Comment évaluez-vous votre niveau avant Digital Campus dans les domaines suivants? [Marketing et communication]",
+  niveau_avt_dc_dev = "Comment évaluez-vous votre niveau avant Digital Campus dans les domaines suivants? [Developpement]",
+  niveau_avt_dc_gest = "Comment évaluez-vous votre niveau avant Digital Campus dans les domaines suivants? [Gestion de projet]",
+  niveau_apr_dc_crea = "Comment évaluez-vous votre niveau après Digital Campus dans les domaines suivants? [Créatif]",
+  niveau_apr_dc_mkt_com = "Comment évaluez-vous votre niveau après Digital Campus dans les domaines suivants? [Marketing et communication]",
+  niveau_apr_dc_dev = "Comment évaluez-vous votre niveau après Digital Campus dans les domaines suivants? [Developpement]",
+  niveau_apr_dc_gest = "Comment évaluez-vous votre niveau après Digital Campus dans les domaines suivants? [Gestion de projet]",
+  spe = "Quelle spécialité avez-vous choisi ?",
+  spe_ok = "Votre spécialité a-t-elle répondu à vos attentes ?",
+  tps_trouve_alt = "En combien de temps avez-vous trouvé votre alternance ?",
+  sourcing_alt = "Comment avez-vous trouvé votre alternance ?",
+  reco_dc = "Recommanderiez-vous Digital Campus à d'autres personnes sur une échelle de 1 à 5 ?",
+  recherche_emploi_apr_dc = "Avez-vous cherché un emploi à la fin de vos études ?",
   non_pq = "Non, pourquoi ?",
-  tps_recherche_emploi = "Combien de temps a dur� votre recherche d'emploi ?",
-  spe_pro = "Quelle est votre sp�cialisation professionnelle ?",
-  type_contrat_apr_dc = "Quel �tait le type de contrat ?",
-  tps_plein_partiel = "�tait-ce � temps plein ou � temps partiel ?",
-  salaire_embauche_apr_dc = "Quel �tait votre salaire � l'embauche ?",
-  emploi_priv_pub = "Votre emploi �tait-il dans le priv� ou dans le public ?",
-  csp_apr_dc = "A quelle cat�gorie socio-professionnelle apparteniez-vous ?",
-  type_entreprise = "Dans quel type d'entreprise �tiez-vous?",
-  dept_premier_emploi = "Dans quel d�partement avez-vous trouv� votre premier emploi ?",
+  tps_recherche_emploi = "Combien de temps a duré votre recherche d'emploi ?",
+  spe_pro = "Quelle est votre spécialisation professionnelle ?",
+  type_contrat_apr_dc = "Quel était le type de contrat ?",
+  tps_plein_partiel = "Était-ce à temps plein ou à temps partiel ?",
+  salaire_embauche_apr_dc = "Quel était votre salaire à l'embauche ?",
+  emploi_priv_pub = "Votre emploi était-il dans le privé ou dans le public ?",
+  csp_apr_dc = "A quelle catégorie socio-professionnelle apparteniez-vous ?",
+  type_entreprise = "Dans quel type d'entreprise étiez-vous?",
+  dept_premier_emploi = "Dans quel département avez-vous trouvé votre premier emploi ?",
   milieu = "Dans quel milieu ?",
-  change_ville_pr_emploi = "Aviez-vous chang� de ville pour d�crocher cet emploi ?",
-  cmt_trouve_emploi_apr_dc = "Comment aviez-vous trouv� votre travail ?",
-  perception_recherche_emploi_apr_dc = "Comment aviez-vous per�u votre recherche d'emploi ?",
-  satisfaction_premier_emploi = "Aviez-vous �t� satisfait de ce premier emploi?",
+  change_ville_pr_emploi = "Aviez-vous changé de ville pour décrocher cet emploi ?",
+  cmt_trouve_emploi_apr_dc = "Comment aviez-vous trouvé votre travail ?",
+  perception_recherche_emploi_apr_dc = "Comment aviez-vous perçu votre recherche d'emploi ?",
+  satisfaction_premier_emploi = "Aviez-vous été satisfait de ce premier emploi?",
   place_travail_vie = "Quelle place occupe votre travail dans votre vie?",
   estimation_importance_dc_recrutement = "A combien estimez vous l'importance de digital campus dans votre recrutement?"
 )
@@ -116,6 +116,10 @@ csv <- csv %>% mutate(formation_DC = ifelse(formation_DC == "Bacelor", "Bachelor
 csv <- csv %>% mutate(ville_formation_DC = ifelse(ville_formation_DC == "lyon", "Lyon",
                                                   ifelse(ville_formation_DC == "paris", "Paris", as.character(ville_formation_DC))))
 
+# Delete annee_naissance = 1904
+csv2 <- with(csv, (annee_naissance == "1904"))
+csv <- csv[!csv2, ]
+
 # Set data as factor.
 csv$nationalite <- as.factor(csv$nationalite)
 csv$genre <- as.factor(csv$genre)
@@ -142,7 +146,8 @@ head(csv)
 # Calcul de l'âge réel et stockage du résultat dans une nouvelle colonne
 csv$age <- 2019 - csv$annee_naissance
 
-data_age <- plot_ly(data = csv, x=~actual_age,type="histogram")
+data_age <- plot_ly(data = csv, x=~age,type="histogram")
+test <- plot_ly(data = csv, x=~age,type="scatter", mode = "lines")
 
 ## SEXE
 data_sexe <- plot_ly(data = csv, x=~genre,type="histogram")
